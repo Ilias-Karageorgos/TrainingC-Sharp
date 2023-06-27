038c62aeef396fb804fcef58c6145a79f6958436
Ilias-Karageorgos <118739697+Ilias-Karageorgos@users.noreply.github.com>
2023-06-27 13:45:15 +0300

    Add files via upload

---
+++ ProjectCar/ProjectCar/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjectCar
+{
+    internal partial class Program
+    {
+        class Car 
+        {
+            private string name;
+
+            public Car(string name)
+            {
+                this.name = name;
+            }
+                      
+            public void Start()
+            {
+                Console.WriteLine("Car started");
+            }
+
+            public string GetName() 
+            {
+                return name;
+            }
+
+
+        }
+    }
+}
---
+++ ProjectCar/ProjectCar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProjectCar
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car("Opel"); 
+
+            c1.Start();
+            Console.WriteLine(c1.GetName());
+            }
+    }
+}
